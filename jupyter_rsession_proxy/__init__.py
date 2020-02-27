920f2a32b91eb6d46fbe83bd78a5a01aff767911
ryanlovett <rylo@berkeley.edu>
2020-02-26 21:56:00 -0800

    Remove shiny proxy.
    It is available at https://github.com/ryanlovett/jupyter-shiny-proxy.

--- jupyter_rsession_proxy/__init__.py
+++ jupyter_rsession_proxy/__init__.py
@@ -1,43 +1,7 @@
 import os
-import tempfile
 import subprocess
 import getpass
 import shutil
-from textwrap import dedent
-
-def setup_shiny():
-    '''Manage a Shiny instance.'''
-
-    name = 'shiny'
-    def _get_shiny_cmd(port):
-        conf = dedent("""
-            run_as {user};
-            server {{
-                listen {port};
-                location / {{
-                    site_dir {site_dir};
-                    log_dir {site_dir}/logs;
-                    directory_index on;
-                }}
-            }}
-        """).format(
-            user=getpass.getuser(),
-            port=str(port),
-            site_dir=os.getcwd()
-        )
-
-        f = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        f.write(conf)
-        f.close()
-        return ['shiny-server', f.name]
-
-    return {
-        'command': _get_shiny_cmd,
-        'launcher_entry': {
-            'title': 'Shiny',
-            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'shiny.svg')
-        }
-    }
 
 def setup_rstudio():
     def _get_rsession_env(port):
