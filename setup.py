a047344056dd0b9e85603c877b6902ad45f5eb03
ryanlovett <rylo@berkeley.edu>
2020-03-03 10:53:38 -0800

    Remove "rsession" from package description.
    Since we're not technically proxying it anymore by default.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='1.0',
     url="https://github.com/jupyterhub/jupyter-rsession-proxy",
     author="Ryan Lovett & Yuvi Panda",
-    description="Jupyter extension to proxy RStudio's rsession",
+    description="Jupyter extension to proxy RStudio",
     packages=setuptools.find_packages(),
 	keywords=['Jupyter'],
 	classifiers=['Framework :: Jupyter'],
