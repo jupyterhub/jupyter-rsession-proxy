42880b3a85513a7845c5abe53cbe08b0f5de5256
ryanlovett <rylo@berkeley.edu>
2020-11-16 16:08:07 -0800

    Set `--www-root-path` from environment variable.

--- jupyter_rsession_proxy/__init__.py
+++ jupyter_rsession_proxy/__init__.py
@@ -33,9 +33,25 @@ def setup_rserver():
     def _get_cmd(port):
         return [
             get_rstudio_executable('rserver'),
+            '--auth-none=1',
+            '--www-frame-origin=same',
             '--www-port=' + str(port),
-            '--www-frame-origin=same'
+            '--www-verify-user-agent=0'
         ]
+
+        # Tell rserver what path it is being served from.
+        # rserver's www-root-path option is present in RStudio >= 1.4. If this
+        # environment variable is set to rserver's default, /, we don't set the
+        # option. The value should be the name of our entry point which is
+        # usually /rstudio/, however since people can add custom entry points,
+        # we won't assume.
+        www_root_path = os.environ.get('RSESSION_PROXY_WWW_ROOT_PATH', '/')
+        if www_root_path != "/":
+            hub_service_prefix = os.environ.get('JUPYTERHUB_SERVICE_PREFIX', '')
+            root_path = pathlib.Path(hub_service_prefix + www_root_path)
+            cmd.append(f'--www-root-path={root_path}/')
+
+        return cmd
 
     return {
         'command': _get_cmd,
