a0b3c849792bc110b48acb92b620d74bdaec192e
ryanlovett <rylo@berkeley.edu>
2021-08-03 11:26:53 -0700

    Install and enable notebook and server extenstions.

--- setup.py
+++ setup.py
@@ -1,24 +1,53 @@
 import setuptools
 
-setuptools.setup(
-    name="jupyter-rsession-proxy",
+# The name of the project
+name = "jupyter_rsession_proxy"
+
+setup_args = dict(
+    name=name.replace("_", "-"),
     version='2.0',
     url="https://github.com/jupyterhub/jupyter-rsession-proxy",
     author="Ryan Lovett & Yuvi Panda",
     description="Jupyter extension to proxy RStudio",
     packages=setuptools.find_packages(),
-    keywords=['Jupyter'],
-    classifiers=['Framework :: Jupyter'],
+    keywords=["Jupyter"],
+    classifiers=[
+        "Framework :: Jupyter",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=[
         'jupyter-server-proxy>=3.1.0'
     ],
-	include_package_data=True,
+    include_package_data=True,
     data_files=[
-        ("etc/jupyter/jupyter_server_config.d",
-            ["jupyter-config/jupyter_server_config.d/jupyter_rsession_proxy.json"]
+        (
+            "share/jupyter/nbextensions/jupyter_rsession_proxy",
+            ["jupyter_rsession_proxy/static/tree.js"]
+        ),
+        (
+            "etc/jupyter/jupyter_notebook_config.d",
+            ["jupyter_rsession_proxy/etc/jupyter-rsession-proxy-notebookserverextension.json"],
+        ),
+        (
+            "etc/jupyter/jupyter_server_config.d",
+            ["jupyter_rsession_proxy/etc/jupyter-rsession-proxy-jupyterserverextension.json"],
+        ),
+        (
+            "etc/jupyter/nbconfig/tree.d",
+            ["jupyter_rsession_proxy/etc/jupyter-rsession-proxy-nbextension.json"],
         ),
     ],
     package_data={
         'jupyter_rsession_proxy': ['icons/rstudio.svg', 'static/*'],
     },
 )
+
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
