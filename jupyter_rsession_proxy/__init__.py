949661cd5a43f73e37f2bd3683427c35547a7918
Matus Kosut <matus.kosut@ntnu.no>
2022-06-08 09:11:22 +0200

    add 10s default timeout and env var

--- jupyter_rsession_proxy/__init__.py
+++ jupyter_rsession_proxy/__init__.py
@@ -98,8 +98,12 @@ def setup_rserver():
 
         return cmd
 
+    def _get_timeout(default=10):
+        return os.getenv('RSERVER_TIMEOUT', default)
+
     server_process = {
         'command': _get_cmd,
+        'timeout': _get_timeout,
         'environment': _get_env,
         'rewrite_response': rewrite_auth,
         'launcher_entry': {
@@ -140,8 +144,12 @@ def setup_rsession():
             '--www-port=' + str(port)
         ]
 
+    def _get_timeout(default=10):
+        return os.getenv('RSESSION_TIMEOUT', default)
+
     return {
         'command': _get_cmd,
+        'timeout': _get_timeout,
         'environment': _get_env,
         'launcher_entry': {
             'title': 'RStudio',
