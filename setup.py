e40ef8de5263cf219b729e4be32dcce539ad86d7
Jens Henrik Goebbert <j.goebbert@fz-juelich.de>
2024-07-08 08:31:14 +0200

    Update install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
 	keywords=['Jupyter'],
 	classifiers=['Framework :: Jupyter'],
     install_requires=[
-        'jupyter-server-proxy>=4.1.0'
+        'jupyter-server-proxy>4.1.0'
     ],
     entry_points={
         'jupyter_serverproxy_servers': [
