5113f4572583bc2ba56aae951d02ec68e1a23841
Sarah Gibson <drsarahlgibson@gmail.com>
2025-01-14 13:52:30 +0000

    Bump to v2.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-rsession-proxy",
-    version='2.2.1',
+    version='2.3.0',
     url="https://github.com/jupyterhub/jupyter-rsession-proxy",
     author="Ryan Lovett & Yuvi Panda",
     description="Jupyter extension to proxy RStudio",
